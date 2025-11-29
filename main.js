ae7dd6021d1434876bfe6fd46963995ac6bd1408
Russel Domingo <russeldomingo099@gmail.com>
2025-11-29 09:52:09 +0800

    adding the tile object

--- main.js
+++ main.js
@@ -1,23 +1,39 @@
-const Tiles = function (){
+const Tiles = function () {
 
-    let row = 3;
-    let col = 3;
-    let board = []
+    let rows = 3;
+    let cols = 3;
+    let board = [];
     let value = "";
 
     function generateBoard() {
-        for (let i = 0; i < row; i++) {
+        for (let i = 0; i < rows; i++) {
             board[i] = [];
-            for (let j = 0; j < col; i++) {
+            for (let j = 0; j < cols; j++) {
                 board[i][j] = value;
             }
         }
     }
 
-    return {
-        generateBoard
+    function getBoard() {
+        return board;
     }
-}
+
+    function setTiles(row, col, value) {
+        if(row < 0 || row >= rows || col < 0 || col >= cols) {
+            return;
+        } else {
+            board[row, col] = value;
+        }
+    }
+
+    generateBoard();
+
+    return {
+        board,
+        getBoard,
+        setTiles,
+    };
+};
 
 const tiles = Tiles();
-console.log(tiles.generateBoard());
+console.log(tiles.board);
