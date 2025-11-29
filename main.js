1b8ddd56e07d4d91a5e9e923b96ff3b24f141bdf
Russel Domingo <russeldomingo099@gmail.com>
2025-11-29 10:35:14 +0800

    Finishing the gameController object

--- main.js
+++ main.js
@@ -71,4 +71,88 @@ const PLayer = (function () {
     return {create};
 }());
 
+const GameController = (function() {
+
+    const board = Tiles;
+    const player1 = PLayer.create("Player1", "X");
+    const player2 = PLayer.create("Player2", "O");
+
+    let currentPlayer = player1;
+
+    function switchTurn() {
+        if(currentPlayer === player1) {
+            currentPlayer = player2;
+        } else {
+            currentPlayer = player1;
+        }
+    }
+
+    function getCurrentPLayer() {
+        return currentPlayer;
+    }
+
+    function playMove(row, col) {
+        board.setTiles(row, col, currentPlayer.getMarker());
+
+        const winner = checkWinner();
+        if(winner) {
+            console.log(winner);
+        }
+        return winner;
+    }
+
+
+    function checkWinner() {
+        const b = board.getBoard();
+
+        for(let i = 0; i < 3; i++) {
+            if(b[i][0] && b[i][0] === b[i][1] && b[i][1] === b[i][2]) {
+                return `${currentPlayer.getName()} wins!`;
+            }
+        }
+
+        for(let i = 0; i < 3; i++) {
+            if(b[0][i] && b[0][i] === b[1][i] && b[1][i] === b[2][i]) {
+                return `${currentPlayer.getName()} wins!`;
+            }
+        }
+
+        if (b[0][0] && b[0][0] === b[1][1] && b[1][1] === b[2][2]) {
+            return `${currentPlayer.getName()} wins!`;
+        }
+        if (b[0][2] && b[0][2] === b[1][1] && b[1][1] === b[2][0]) {
+            return `${currentPlayer.getName()} wins!`;
+        }
+
+        return null;
+    }
+
+    function resetGame() {
+        board.resetBoard();
+        currentPlayer = player1;
+    }
+    return {
+        getCurrentPLayer,
+        playMove,
+        switchTurn,
+        checkWinner,
+    };
+})();
+
 console.log(Tiles.getBoard());
+console.log("Starting Tic-Tac-Toe game:\n");
+
+console.log("Player1 (X) moves at (0,0)");
+GameController.playMove(0,0);
+
+console.log("Player2 (O) moves at (1,1)");
+GameController.playMove(1,1);
+
+console.log("Player1 (X) moves at (0,1)");
+GameController.playMove(0,1);
+
+console.log("Player2 (O) moves at (2,2)");
+GameController.playMove(2,2);
+
+console.log("Player1 (X) moves at (0,2)");
+GameController.playMove(0,2); // Player1 should win
